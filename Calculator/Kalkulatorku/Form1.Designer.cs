abcdb071af43e49182a6e46ec158218d2407eaed
Jeptika Herni N <58242360+jeptika@users.noreply.github.com>
2020-05-26 19:55:57 +0700

    Add files via upload

---
+++ Calculator/Kalkulatorku/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace Kalkulatorku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tujuh = new System.Windows.Forms.Button();
+            this.delapan = new System.Windows.Forms.Button();
+            this.sembilan = new System.Windows.Forms.Button();
+            this.bagi = new System.Windows.Forms.Button();
+            this.ce = new System.Windows.Forms.Button();
+            this.c = new System.Windows.Forms.Button();
+            this.kali = new System.Windows.Forms.Button();
+            this.enam = new System.Windows.Forms.Button();
+            this.lima = new System.Windows.Forms.Button();
+            this.empat = new System.Windows.Forms.Button();
+            this.kurang = new System.Windows.Forms.Button();
+            this.tiga = new System.Windows.Forms.Button();
+            this.dua = new System.Windows.Forms.Button();
+            this.satu = new System.Windows.Forms.Button();
+            this.samadengan = new System.Windows.Forms.Button();
+            this.tambah = new System.Windows.Forms.Button();
+            this.titik = new System.Windows.Forms.Button();
+            this.nol = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tujuh
+            // 
+            this.tujuh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.tujuh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tujuh.Location = new System.Drawing.Point(13, 75);
+            this.tujuh.Name = "tujuh";
+            this.tujuh.Size = new System.Drawing.Size(45, 45);
+            this.tujuh.TabIndex = 0;
+            this.tujuh.Text = "7";
+            this.tujuh.UseVisualStyleBackColor = false;
+            this.tujuh.Click += new System.EventHandler(this.click_button);
+            // 
+            // delapan
+            // 
+            this.delapan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.delapan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delapan.Location = new System.Drawing.Point(64, 75);
+            this.delapan.Name = "delapan";
+            this.delapan.Size = new System.Drawing.Size(45, 45);
+            this.delapan.TabIndex = 1;
+            this.delapan.Text = "8";
+            this.delapan.UseVisualStyleBackColor = false;
+            this.delapan.Click += new System.EventHandler(this.click_button);
+            // 
+            // sembilan
+            // 
+            this.sembilan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.sembilan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sembilan.Location = new System.Drawing.Point(115, 75);
+            this.sembilan.Name = "sembilan";
+            this.sembilan.Size = new System.Drawing.Size(45, 45);
+            this.sembilan.TabIndex = 2;
+            this.sembilan.Text = "9";
+            this.sembilan.UseVisualStyleBackColor = false;
+            this.sembilan.Click += new System.EventHandler(this.click_button);
+            // 
+            // bagi
+            // 
+            this.bagi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.bagi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bagi.Location = new System.Drawing.Point(166, 75);
+            this.bagi.Name = "bagi";
+            this.bagi.Size = new System.Drawing.Size(45, 45);
+            this.bagi.TabIndex = 3;
+            this.bagi.Text = "/";
+            this.bagi.UseVisualStyleBackColor = false;
+            this.bagi.Click += new System.EventHandler(this.operator_click);
+            // 
+            // ce
+            // 
+            this.ce.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ce.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ce.Location = new System.Drawing.Point(217, 75);
+            this.ce.Name = "ce";
+            this.ce.Size = new System.Drawing.Size(45, 45);
+            this.ce.TabIndex = 4;
+            this.ce.Text = "CE";
+            this.ce.UseVisualStyleBackColor = false;
+            this.ce.Click += new System.EventHandler(this.ce_Click);
+            // 
+            // c
+            // 
+            this.c.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.c.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c.Location = new System.Drawing.Point(217, 126);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(45, 45);
+            this.c.TabIndex = 9;
+            this.c.Text = "C";
+            this.c.UseVisualStyleBackColor = false;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // kali
+            // 
+            this.kali.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.kali.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kali.Location = new System.Drawing.Point(166, 126);
+            this.kali.Name = "kali";
+            this.kali.Size = new System.Drawing.Size(45, 45);
+            this.kali.TabIndex = 8;
+            this.kali.Text = "*";
+            this.kali.UseVisualStyleBackColor = false;
+            this.kali.Click += new System.EventHandler(this.operator_click);
+            // 
+            // enam
+            // 
+            this.enam.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.enam.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enam.Location = new System.Drawing.Point(115, 126);
+            this.enam.Name = "enam";
+            this.enam.Size = new System.Drawing.Size(45, 45);
+            this.enam.TabIndex = 7;
+            this.enam.Text = "6";
+            this.enam.UseVisualStyleBackColor = false;
+            this.enam.Click += new System.EventHandler(this.click_button);
+            // 
+            // lima
+            // 
+            this.lima.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lima.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lima.Location = new System.Drawing.Point(64, 126);
+            this.lima.Name = "lima";
+            this.lima.Size = new System.Drawing.Size(45, 45);
+            this.lima.TabIndex = 6;
+            this.lima.Text = "5";
+            this.lima.UseVisualStyleBackColor = false;
+            this.lima.Click += new System.EventHandler(this.click_button);
+            // 
+            // empat
+            // 
+            this.empat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.empat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.empat.Location = new System.Drawing.Point(13, 126);
+            this.empat.Name = "empat";
+            this.empat.Size = new System.Drawing.Size(45, 45);
+            this.empat.TabIndex = 5;
+            this.empat.Text = "4";
+            this.empat.UseVisualStyleBackColor = false;
+            this.empat.Click += new System.EventHandler(this.click_button);
+            // 
+            // kurang
+            // 
+            this.kurang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.kurang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kurang.Location = new System.Drawing.Point(166, 177);
+            this.kurang.Name = "kurang";
+            this.kurang.Size = new System.Drawing.Size(45, 45);
+            this.kurang.TabIndex = 13;
+            this.kurang.Text = "-";
+            this.kurang.UseVisualStyleBackColor = false;
+            this.kurang.Click += new System.EventHandler(this.operator_click);
+            // 
+            // tiga
+            // 
+            this.tiga.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.tiga.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tiga.Location = new System.Drawing.Point(115, 177);
+            this.tiga.Name = "tiga";
+            this.tiga.Size = new System.Drawing.Size(45, 45);
+            this.tiga.TabIndex = 12;
+            this.tiga.Text = "3";
+            this.tiga.UseVisualStyleBackColor = false;
+            this.tiga.Click += new System.EventHandler(this.click_button);
+            // 
+            // dua
+            // 
+            this.dua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.dua.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dua.Location = new System.Drawing.Point(64, 177);
+            this.dua.Name = "dua";
+            this.dua.Size = new System.Drawing.Size(45, 45);
+            this.dua.TabIndex = 11;
+            this.dua.Text = "2";
+            this.dua.UseVisualStyleBackColor = false;
+            this.dua.Click += new System.EventHandler(this.click_button);
+            // 
+            // satu
+            // 
+            this.satu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.satu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.satu.Location = new System.Drawing.Point(13, 177);
+            this.satu.Name = "satu";
+            this.satu.Size = new System.Drawing.Size(45, 45);
+            this.satu.TabIndex = 10;
+            this.satu.Text = "1";
+            this.satu.UseVisualStyleBackColor = false;
+            this.satu.Click += new System.EventHandler(this.click_button);
+            // 
+            // samadengan
+            // 
+            this.samadengan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.samadengan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.samadengan.Location = new System.Drawing.Point(217, 177);
+            this.samadengan.Name = "samadengan";
+            this.samadengan.Size = new System.Drawing.Size(45, 96);
+            this.samadengan.TabIndex = 19;
+            this.samadengan.Text = "=";
+            this.samadengan.UseVisualStyleBackColor = false;
+            this.samadengan.Click += new System.EventHandler(this.samadengan_Click);
+            // 
+            // tambah
+            // 
+            this.tambah.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.tambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tambah.Location = new System.Drawing.Point(166, 228);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(45, 45);
+            this.tambah.TabIndex = 18;
+            this.tambah.Text = "+";
+            this.tambah.UseVisualStyleBackColor = false;
+            this.tambah.Click += new System.EventHandler(this.operator_click);
+            // 
+            // titik
+            // 
+            this.titik.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.titik.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titik.Location = new System.Drawing.Point(115, 228);
+            this.titik.Name = "titik";
+            this.titik.Size = new System.Drawing.Size(45, 45);
+            this.titik.TabIndex = 17;
+            this.titik.Text = ",";
+            this.titik.UseVisualStyleBackColor = false;
+            this.titik.Click += new System.EventHandler(this.click_button);
+            // 
+            // nol
+            // 
+            this.nol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.nol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nol.Location = new System.Drawing.Point(13, 228);
+            this.nol.Name = "nol";
+            this.nol.Size = new System.Drawing.Size(96, 45);
+            this.nol.TabIndex = 15;
+            this.nol.Text = "0";
+            this.nol.UseVisualStyleBackColor = false;
+            this.nol.Click += new System.EventHandler(this.click_button);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 36);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(275, 329);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.samadengan);
+            this.Controls.Add(this.tambah);
+            this.Controls.Add(this.titik);
+            this.Controls.Add(this.nol);
+            this.Controls.Add(this.kurang);
+            this.Controls.Add(this.tiga);
+            this.Controls.Add(this.dua);
+            this.Controls.Add(this.satu);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.kali);
+            this.Controls.Add(this.enam);
+            this.Controls.Add(this.lima);
+            this.Controls.Add(this.empat);
+            this.Controls.Add(this.ce);
+            this.Controls.Add(this.bagi);
+            this.Controls.Add(this.sembilan);
+            this.Controls.Add(this.delapan);
+            this.Controls.Add(this.tujuh);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tujuh;
+        private System.Windows.Forms.Button delapan;
+        private System.Windows.Forms.Button sembilan;
+        private System.Windows.Forms.Button bagi;
+        private System.Windows.Forms.Button ce;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Button kali;
+        private System.Windows.Forms.Button enam;
+        private System.Windows.Forms.Button lima;
+        private System.Windows.Forms.Button empat;
+        private System.Windows.Forms.Button kurang;
+        private System.Windows.Forms.Button tiga;
+        private System.Windows.Forms.Button dua;
+        private System.Windows.Forms.Button satu;
+        private System.Windows.Forms.Button samadengan;
+        private System.Windows.Forms.Button tambah;
+        private System.Windows.Forms.Button titik;
+        private System.Windows.Forms.Button nol;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Calculator/Kalkulatorku/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulatorku
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        String operation = "";
+        bool isOperation = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void click_button(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperation))
+                textBox1.Clear();
+
+            isOperation = false;
+            Button button = (Button)sender;
+            if (button.Text == ",")
+            {
+                if(!textBox1.Text.Contains(","))
+                    textBox1.Text = textBox1.Text + button.Text;
+            }
+            else
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+        
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (resultValue != 0)
+            {
+                samadengan.PerformClick();
+                operation = button.Text;
+                label1.Text = resultValue + " " + operation;
+                isOperation = true;
+            }
+            else
+            {
+                operation = button.Text;
+                resultValue = Double.Parse(textBox1.Text);
+                label1.Text = resultValue + " " + operation;
+                isOperation = true; 
+            }
+        }
+
+        private void ce_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            resultValue = 0;
+        }
+
+        private void samadengan_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    textBox1.Text = (resultValue + Double.Parse(textBox1.Text)).ToString();
+                    break;
+
+                case "-":
+                    textBox1.Text = (resultValue - Double.Parse(textBox1.Text)).ToString();
+                    break;
+
+                case "/":
+                    textBox1.Text = (resultValue / Double.Parse(textBox1.Text)).ToString();
+                    break;
+
+                case "*":
+                    textBox1.Text = (resultValue * Double.Parse(textBox1.Text)).ToString();
+                    break;
+
+                default:
+                    break;
+
+            }
+            resultValue = Double.Parse(textBox1.Text);
+            label1.Text = "";
+        }
+    }
+}
